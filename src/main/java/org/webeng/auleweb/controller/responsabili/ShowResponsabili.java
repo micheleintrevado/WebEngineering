9c26e79ca381d2ca5240f8aac8e625d929a07aad
Enrico Adamelli <enricosimone.adamelli@student.univaq.it>
2024-08-12 16:21:47 +0200

    Responsabili controller

---
+++ src/main/java/org/webeng/auleweb/controller/responsabili/ShowResponsabili.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.webeng.auleweb.controller.responsabili;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.webeng.auleweb.application.AulewebBaseController;
+
+/**
+ *
+ * @author enric
+ */
+public class ShowResponsabili extends AulewebBaseController{
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
